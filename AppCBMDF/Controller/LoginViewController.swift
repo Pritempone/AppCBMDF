595cd71ccd0a1380a6a1ab08826f98d3ed60a597
Pritempone <103539007+Pritempone@users.noreply.github.com>
2022-11-16 11:29:16 -0300

    Logar

--- AppCBMDF/Controller/LoginViewController.swift
+++ AppCBMDF/Controller/LoginViewController.swift
@@ -17,7 +17,7 @@ class LoginViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Entrar"
+        self.title = "Logar"
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
 }
