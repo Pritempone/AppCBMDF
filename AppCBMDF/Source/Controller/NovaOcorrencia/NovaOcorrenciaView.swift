b7541fd77219267457d5a9c88fdd7443eafadaf0
Pritempone <103539007+Pritempone@users.noreply.github.com>
2022-11-24 10:05:32 -0300

    Tabbar

---
+++ AppCBMDF/Source/Controller/NovaOcorrencia/NovaOcorrenciaView.swift
@@ -0,0 +1,25 @@
+//
+//  NovaOcorrenciaView.swift
+//  AppCBMDF
+//
+//  Created by IFB BIOTIC 03 on 24/11/22.
+//
+
+import Foundation
+
+import UIKit
+
+class NovaOcorrenciaView: ViewDefault {
+    //MARK: - Closures
+    
+    //MARK: - Proports
+    
+    //MARK: - Inits
+    
+    
+    
+   override  func setupVisualElements() {
+
+    }
+    
+}
